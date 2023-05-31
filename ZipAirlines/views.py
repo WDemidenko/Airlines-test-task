b2efae366c5085a87b9a0ae85bfab31851ee0356
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-05-31 23:00:50 +0200

    Add viewset for Plane

--- ZipAirlines/views.py
+++ ZipAirlines/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from ZipAirlines.models import Plane
+from ZipAirlines.serializers import PlaneSerializer, PlaneDetailSerializer
+
+
+class PlaneViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Plane.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return PlaneDetailSerializer
+        return PlaneSerializer
